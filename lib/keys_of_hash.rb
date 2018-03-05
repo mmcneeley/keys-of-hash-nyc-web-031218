0ef62eaee74a688b8c94f05acb21bbd17ceb5773
Mike McNeeley <mmcneeley@gmail.com>
2018-03-05 01:10:32 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,13 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+
+  def keys_of(*args)
+      collect do |key, value|
+        if args.include?(value)
+          key
+        else
+          nil
+        end
+      end.compact
   end
+
 end
